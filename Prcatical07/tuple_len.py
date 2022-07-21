f1c131edda81cd943caba16ffe1a7075d1e95352
Gaur777 <99119577+Gaur777@users.noreply.github.com>
2022-07-21 14:35:54 +0530

    Add files via upload

---
+++ Prcatical07/tuple_len.py
@@ -0,0 +1,9 @@
+#program too find lenght of tuple
+
+tuple1=[2,6,32,76,22,5,11]
+count =0
+n = len(tuple1)
+for i in range(0,n):
+    count = count+1
+print("Given tuple :",tuple1)
+print("Lenght of tuple:",count)
